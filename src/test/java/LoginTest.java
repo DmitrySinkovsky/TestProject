e59bce04059f74725c278eee3342c2daac91d4ac
DmitrySinkovsky <sinkovsky1993@gmail.com>
2018-04-21 20:10:59 +0300

    test commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -32,7 +32,7 @@ public class LoginTest {
     public void LoginTest(){
         driver.findElement(By.cssSelector("li[class='new-sign-in']")).click();
         sleep(5);
-        driver.findElement(By.cssSelector("input[id ='exampleInputEmail1']")).sendKeys("lihoy@singree.com");
+        driver.findElement(By.cssSelector("input[id ='exampleInputEmail1']")).sendKeys("test@test.com");
         driver.findElement(By.cssSelector("input[id='exampleInputPassword1']")).sendKeys("1111111");
         sleep(2);
         driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[1]/div/div/div[2]/div/div/form[2]/button")).click();
